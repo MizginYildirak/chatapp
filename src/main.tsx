5cb89f6351e5609eed0aeab42f18a5ebd87af120
MizginYildirak <mizgin_yldrak@hotmail.com>
2025-03-02 00:04:33 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+[
+  {
+    "_id": "59ef4a83ee8364808d761beb",
+    "index": 0,
+    "guid": "e50ffae9-7128-4148-9ee5-40c3fc523c5d",
+    "isActive": false,
+    "balance": "$2,341.81",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "ey
